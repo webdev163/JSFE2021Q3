f35e39bc0e27fb9b6a9b645e7c908a977a3cb3f1
Dmitry K <69163014+webdev163@users.noreply.github.com>
2021-12-02 17:06:05 +0400

    feat: rename js->ts, change indents

---
+++ migration-newip-to-ts/src/components/controller/appLoader.ts
@@ -0,0 +1,11 @@
+import Loader from './loader';
+
+class AppLoader extends Loader {
+  constructor() {
+    super('https://newsapi.org/v2/', {
+      apiKey: '2f64c80d64dc4164889c06bac3af50be',
+    });
+  }
+}
+
+export default AppLoader;
